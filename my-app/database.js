694c63fe41e85b33479448359b7697b7434a337a
O Dodart <43223613+Os-tasty-code@users.noreply.github.com>
2022-04-07 19:27:17 -0400

    database connection

--- my-app/database.js
+++ my-app/database.js
@@ -2,15 +2,14 @@
   UNLESS YOU ARE USING THE GMU VPN*/
 
 const { MongoClient } = require('mongodb');
-//needs to be changed with Zach's database information / user information
-const uri = "mongodb+srv://oddcosmo:RTFR2kNSxewwtpb@cluster0.v3ihi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://O:PRLJYAb3wnN6QBlL@cluster0.wdjd3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
     //db --> access database
-  const db = client.db("family");
+  const db = client.db("ChoreApp");
   //collection --> access collection within database (usually information about actions on database)
-  var cursor = db.collection('house_example').find({});
+  var cursor = db.collection('goals').find({});
   console.log(cursor);
 
   client.close();
