54018a07b0d0c95a7f057479b950996e78aeae1e
jameslroche <99369770+jameslroche@users.noreply.github.com>
2022-04-21 14:01:42 -0400

    Update App.js
    MANUALLY UPLOADED-
    basic change is now the page pulls the chore data from express rather than creating it on its own

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import { unmountComponentAtNode} from "react-dom";
+import { unmountComponentAtNode } from "react-dom";
 import './App.css'
 //website: https://www.freecodecamp.org/news/create-a-react-frontend-a-node-express-backend-and-connect-them-together-c5798926047c/
 //used for connecting React with Express
 
-//import axios from 'axios'
+import axios from 'axios'
 //const URL = 'https://jsonplaceholder.typicode.com/users'
 
 //connecting Express and React
@@ -44,30 +44,46 @@ const Table = () => {
 
 	const getData = async () => {
 		// const response = await axios.get(URL)
-		const response = {
-			// Ideally these would have a unique ID for each one.
-			data: [
-				{
-					"id": 1,
-					"name": "Clean dishes",
-					"assignedTo": "James",
-					"desc": "Clean all dishes in the sink."
-				},
-				{
-					"id": 2,
-					"name": "Do laundry",
-					"assignedTo": "O",
-					"desc": "Do everyone's laundry, wash and dry."
-				},
-				{
-					"id": 3,
-					"name": "Vaccum",
-					"assignedTo": "Zach",
-					"desc": "Vaccum the basement and first floor."
-				},
-			]
-		}
-		setData(response.data)
+		// const response = {
+		// 	// Ideally these would have a unique ID for each one.
+		// 	data: [
+		// 		{
+		// 			"id": 1,
+		// 			"name": "Clean dishes",
+		// 			"assignedTo": "James",
+		// 			"desc": "Clean all dishes in the sink."
+		// 		},
+		// 		{
+		// 			"id": 2,
+		// 			"name": "Do laundry",
+		// 			"assignedTo": "O",
+		// 			"desc": "Do everyone's laundry, wash and dry."
+		// 		},
+		// 		{
+		// 			"id": 3,
+		// 			"name": "Vaccum",
+		// 			"assignedTo": "Zach",
+		// 			"desc": "Vaccum the basement and first floor."
+		// 		},
+		// 	]
+		// }
+
+		// Make a request for a user with a given ID
+		axios.get('http://localhost:9000/api')
+			.then(function (responseAx) {
+				// handle success
+				console.log(responseAx);
+				setData(responseAx.data)
+			})
+			.catch(function (error) {
+				// handle error
+				console.log(error);
+			})
+			.then(function () {
+				// always executed
+			});
+
+		//setData(response.data)
 	}
 
 	const removeData = (id) => {
@@ -89,19 +105,19 @@ const Table = () => {
 		// Make sure we have a unique ID.
 		// Just generate it instead of trying to force the user to make one.
 		let inputNum = Number(input.id);
-		for (let i=0; i<data.length; i++) {
-			if (Number(data[i].id) === inputNum){ return }
+		for (let i = 0; i < data.length; i++) {
+			if (Number(data[i].id) === inputNum) { return }
 		}
-		setData(data => [...data,input]);
+		setData(data => [...data, input]);
 	}
 	const renderHeader = () => {
 		return (
 			<tr>
-			<th key={"Input"+1}>Chore ID</th>
-			<th key={"Input"+2}>Chore Name</th>
-			<th key={"Input"+3}>Assigned To</th>
-			<th key={"Input"+4}>Description</th>
-			<th key={"Input"+5}>Operation</th>
+				<th key={"Input" + 1}>Chore ID</th>
+				<th key={"Input" + 2}>Chore Name</th>
+				<th key={"Input" + 3}>Assigned To</th>
+				<th key={"Input" + 4}>Description</th>
+				<th key={"Input" + 5}>Operation</th>
 			</tr>
 		)
 	}
@@ -135,39 +151,39 @@ const Table = () => {
 			<br></br>
 			Add a new chore:
 			<br></br></div>)
-		
+
 		output.push(
 			<div>
-			ID:
-			<input
-				type = "number"
-				name = "id"
-				min = {0}
-				value = {input.id}
-				onChange = {handleChange} />
-			<br></br>
-			Chore Name:
-			<input
-				type = "text"
-				name = "name"
-				value = {input.name}
-				onChange = {handleChange} />
-			<br></br>
-			Assigned To:
-			<input
-				type = "text"
-				name = "assignedTo"
-				value = {input.assignedTo}
-				onChange = {handleChange} />
-			<br></br>
-			Description:
-			<input
-				type = "text"
-				name = "desc"
-				value = {input.desc}
-				onChange = {handleChange} />
-			<br></br>
-			<button className='button' class='addButton' onClick={() => addData()}>Add</button>
+				ID:
+				<input
+					type="number"
+					name="id"
+					min={0}
+					value={input.id}
+					onChange={handleChange} />
+				<br></br>
+				Chore Name:
+				<input
+					type="text"
+					name="name"
+					value={input.name}
+					onChange={handleChange} />
+				<br></br>
+				Assigned To:
+				<input
+					type="text"
+					name="assignedTo"
+					value={input.assignedTo}
+					onChange={handleChange} />
+				<br></br>
+				Description:
+				<input
+					type="text"
+					name="desc"
+					value={input.desc}
+					onChange={handleChange} />
+				<br></br>
+				<button className='button' class='addButton' onClick={() => addData()}>Add</button>
 			</div>
 		)
 		return output;
@@ -191,18 +207,18 @@ const Table = () => {
 	return (
 		<>
 			<center>
-			<h1 id='title'>Chore List</h1>
-			<table id='data'>
-				<thead>
-					{renderHeader()}
-				</thead>
-				<tbody>
-					{renderBody()}
-			</tbody>
-			</table>
-			
-			{renderInputs()}
-			{changeWebpage()}
+				<h1 id='title'>Chore List</h1>
+				<table id='data'>
+					<thead>
+						{renderHeader()}
+					</thead>
+					<tbody>
+						{renderBody()}
+					</tbody>
+				</table>
+
+				{renderInputs()}
+				{changeWebpage()}
 			</center>
 		</>
 	)
