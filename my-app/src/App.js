0b18fdd185b049f601a48611542d7d3d7f0408c0
pirey5 <100731773+pirey5@users.noreply.github.com>
2022-04-30 22:59:23 -0400

    Removed ID and cleaned up code
    ID is now handled by MongoDB

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,164 +1,142 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 import './App.css'
 import axios from 'axios'
 
-const URL = 'ws://localhost:9000/api'
-const Table = () => {
-	const [ws, setWs] = useState(new WebSocket(URL));
-
-	const [message] = useState([]);
-	const [messages, setMessages] = useState([]);
-
-	const [data, setData] = useState([])
-	const [input, setInput] = useState({
-		id: 1,
+const baseURL = 'localhost:9000'
+const socketURL = 'ws://' + baseURL + '/api'
+function ChoreTable() {
+	// Handles the chore data.
+	const [data, setData] = React.useState([])
+	const [input, setInput] = React.useState({
+		//id: 1,
 		name: "",
 		assignedTo: "",
 		desc: ""
 	})
-	useEffect(() => {
+
+	React.useEffect(() => {
 		getData()
 	}, [])
 
-	// Set ID to the next highest number initially
-	/*let setHighest = false
-	function loadHighest() {
-		if (!setHighest) {
-			let highest = 0;
-			for (let i=0; i<data.length; i++) {
-				let num = Number(data[i].id)
-				if (num > highest){ highest = num }
-			}
-			
-			setInput({
-				...input,
-				["id"]: highest+1
-			})
-
-			console.log(input.id)
-			setHighest = true
-		}
-	}*/
-
-	useEffect(() => {
-		ws.onopen = () => {
-			console.log('WebSocket Connected');
-		}
+	// Part of the websocket code.
+	const [ws, setWs] = React.useState(new WebSocket(socketURL))
+	const [message] = React.useState([])
+	const [messages, setMessages] = React.useState([])
 	
+	// Establish a websocket so we can send the client data after they've already loaded the page
+	// Used to update the page when the database entries change
+	React.useEffect(() => {
+		ws.onopen = () => {
+			console.log('WebSocket Connected')
+		}
+
 		ws.onmessage = (e) => {
 			console.log(e.data)
 			console.log(JSON.parse(e.data))
 			var parsedData = JSON.parse(e.data)
-		  setData(parsedData.data);
-		  setMessages([message, ...messages]);
+			setData(parsedData.data)
+			setMessages([message, ...messages])
 		}
 	
 		return () => {
 			ws.onclose = () => {
-				console.log('WebSocket Disconnected');
-				setWs(new WebSocket(URL));
+				console.log('WebSocket Disconnected')
+				setWs(new WebSocket(socketURL))
 			}
 		}
-	}, [ws.onmessage, ws.onopen, ws.onclose, messages]);
+	}, [ws.onmessage, ws.onopen, ws.onclose, messages])
 
-
-	const getData = async () => {
-		// Make a request for a user with a given ID
-		axios.get('http://localhost:9000/api')
+	async function getData() {
+		// Make a request for a user with a given chore ID
+		axios.get('http://' + baseURL + '/api')
 			.then(function (responseAx) {
-				// handle success
-				console.log(responseAx);
+				// Handle success
+				console.log(responseAx)
 				setData(responseAx.data)
 			})
 			.catch(function (error) {
-				// handle error
-				console.log(error);
+				// Handle error
+				console.log(error)
 			})
 			.then(function () {
-				// always executed
-			});
+				// Always executed
+			})
 
 		//setData(response.data)
 	}
 
-	const removeData = (id) => {
-
-		const del = data.filter(data => (id !== data.id))
+	function removeData(_id) {
+		const del = data.filter(data => (_id !== data._id))
 		setData(del)
 
 		// Send the delete request to the database.
-		const toSend = {id: id,}
-		axios.post('http://localhost:9000/delete', toSend).then(() => {
+		const toSend = {_id: _id,}
+		axios.post('http://' + baseURL + '/delete', toSend).then(() => {
 			console.log('Chore deletion sent.')
 			}).catch(err => {
-		  		console.error(err);
-			});
+		  		console.error(err)
+			})
 	}
-	const addData = (id) => {
-		// Probably a nicer way to iteratively check these.
-		if (input.id === "") { return }
+
+	function addData() {
+		// There's probably a nicer way to iteratively check these.
+		//if (input.id === "") { return }
 		if (input.name === "") { return }
 		if (input.assignedTo === "") { return }
 		if (input.desc === "") { return }
 
-		// Make sure we have a unique ID.
-		// Just generate it instead of trying to force the user to make one.
-		let inputNum = Number(input.id);
-		for (let i = 0; i < data.length; i++) {
-			if (Number(data[i].id) === inputNum) { return }
-		}
-		setData(data => [...data, input]);
+		setData(data => [...data, input])
 
 		const toSend = {
-			id: input.id,
 			name: input.name,
 			assignedTo: input.assignedTo,
 			desc: input.desc
 		}
 
-		axios.post('http://localhost:9000/create', toSend).then(() => {
+		axios.post('http://' + baseURL + '/create', toSend).then(() => {
 			console.log('Chore addition sent.')
 			}).catch(err => {
-		  		console.error(err);
-			});
+		  		console.error(err)
+			})
 	}
-	const renderHeader = () => {
+
+	function renderHeader() {
 		return (
 			<tr>
-				<th key={"Input" + 1}>Chore ID</th>
-				<th key={"Input" + 2}>Chore Name</th>
-				<th key={"Input" + 3}>Assigned To</th>
-				<th key={"Input" + 4}>Description</th>
-				<th key={"Input" + 5}>Operation</th>
+				<th key={"Input" + 1}>Chore Name</th>
+				<th key={"Input" + 2}>Assigned To</th>
+				<th key={"Input" + 3}>Description</th>
+				<th key={"Input" + 4}>Operation</th>
 			</tr>
 		)
 	}
-	const renderBody = () => {
-		return data && data.map(({ id, name, assignedTo, desc }) => {
+
+	function renderBody() {
+		return data && data.map(({ _id, name, assignedTo, desc }) => {
 			return (
-				<tr key={id}>
-					<td>{id}</td>
+				<tr key={_id}>
 					<td>{name}</td>
 					<td>{assignedTo}</td>
 					<td>{desc}</td>
 					<td className='operation'>
-						<button className='button' type='button' class='deleteButton' onClick={() => removeData(id)}>Delete</button>
+						<button className='button' type='button' class='deleteButton' onClick={() => removeData(_id)}>Delete</button>
 					</td>
 				</tr>
 			)
 		})
 	}
 
-	const handleChange = evt => {
-		const name = evt.target.name;
-		const value = evt.target.value;
+	function handleChange(event) {
+		const name = event.target.name
+		const value = event.target.value
 		setInput({
 			...input,
 			[name]: value
 		})
 	}
-	const renderInputs = () => {
+
+	function renderInputs() {
 		var output = []
 		output.push(<div>
 			<br></br>
@@ -167,14 +145,6 @@ const Table = () => {
 
 		output.push(
 			<div>
-				ID:
-				<input
-					type="number"
-					name="id"
-					min={0}
-					value={input.id}
-					onChange={handleChange} />
-				<br></br>
 				Chore Name:
 				<input
 					type="text"
@@ -199,26 +169,26 @@ const Table = () => {
 				<button className='button' type='button' class='addButton' onClick={() => addData()}>Add</button>
 			</div>
 		)
-		return output;
+		return output
 	}
 
 	return (
-		<>
-			<center>
-				<h1 id='title'>Chore List</h1>
-				<table id='data'>
-					<thead>
-						{renderHeader()}
-					</thead>
-					<tbody>
-						{renderBody()}
-					</tbody>
-				</table>
+		<><center>
+			<h1 id='title'>Chore List</h1>
+			<table id='data'>
+				<thead data-testid='header'>
+					{renderHeader()}
+				</thead>
+				<tbody data-testid='body'>
+					{renderBody()}
+				</tbody>
+			</table>
 
-				{renderInputs()}
-			</center>
-		</>
+			<div data-testid='inputs'>
+			{renderInputs()}
+			</div>
+		</center></>
 	)
 }
 
-export default Table
+export default ChoreTable
